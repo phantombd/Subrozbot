5838f3c5a6743db60c15c225d069196c25b6deec
Karma-goku <64586169+Karma-goku@users.noreply.github.com>
2020-06-07 20:11:49 +0530

    Add files via upload

---
+++ userbot/plugins/hack.py
@@ -0,0 +1,64 @@
+"""Emoji
+Available Commands:
+.emoji shrug
+.emoji apple
+.emoji :/
+.emoji -_-"""
+
+from telethon import events
+
+import asyncio
+from uniborg.util import admin_cmd
+from telethon.tl.functions.users import GetFullUserRequest
+
+
+
+@borg.on(admin_cmd(pattern=r"hack"))
+
+async def _(event):
+
+    if event.fwd_from:
+
+        return
+
+    animation_interval = 3
+
+    animation_ttl = range(0, 11)
+
+    #input_str = event.pattern_match.group(1)
+
+    #if input_str == "hack":
+
+    if event.reply_to_msg_id:
+        reply_message = await event.get_reply_message()
+        replied_user = await event.client(GetFullUserRequest(reply_message.from_id))
+        firstname = replied_user.user.first_name
+        usname = replied_user.user.username
+        idd = reply_message.from_id
+        if idd==7295966789:
+            await event.edit("This is My Master\nI can't hack my master's Account")
+        else:
+            await event.edit("Hacking..")
+            animation_chars = [
+        
+            "`Connecting To Hacked Private Server...`",
+            "`Target Selected.`",
+            "`Hacking... 0%\n▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 4%\n█▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 8%\n██▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",    
+            "`Hacking... 20%\n█████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 36%\n█████████▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 52%\n█████████████▒▒▒▒▒▒▒▒▒▒▒▒ `",
+            "`Hacking... 84%\n█████████████████████▒▒▒▒ `",
+            "`Hacking... 100%\n█████████HACKED███████████ `",
+            "`Targeted Account Hacked...\n\nPay ₹10,00,00,00,000 To`[『𝙆𝘼𝙍𝙈𝘼』](@KarmaBoii) `To Remove this hack..`"
+            ]
+
+            for i in animation_ttl:
+
+                await asyncio.sleep(animation_interval)
+
+                await event.edit(animation_chars[i % 11])
+    else:
+        await event.edit("No User is Defined\n Can't hack account")
+            
