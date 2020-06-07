f658f1b6c9c9af166db0c697018b5d1fe8e28407
Karma-goku <64586169+Karma-goku@users.noreply.github.com>
2020-06-07 21:02:48 +0530

    Update memes.py

--- userbot/plugins/memes.py
+++ userbot/plugins/memes.py
@@ -860,7 +860,7 @@ async def vapor(vpr):
 @register(outgoing=True, pattern="^.repo$")
 async def source(e):
     if not e.text[0].isalpha() and e.text[0] not in ("/", "#", "@", "!"):
-        await e.edit("Ask From Creator Of This Bot, Cozz This iz For Peru Users Only [『𝙆𝘼𝙍𝙈𝘼』](@Karmaboii)")
+        await e.edit("**Ask  From  Creator  Of  This  Bot,  Cozz  This  iz  For  Peru  Users  Only** [『𝙆𝘼𝙍𝙈𝘼』](@Karmaboii)")
 			  
 			  
 @register(outgoing=True, pattern="^.str(?: |$)(.*)")
